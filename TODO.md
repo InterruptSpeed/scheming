01d16cd92e23717d71ca79662d974abd97b6b3ff
Steven Docherty <sdocherty@InterruptSpeed.com>
2020-05-11 20:19:40 -0400

    updated TODO; added type hints

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
++ put proper documentation in for each function
 + pytest unit tests
 + pip packaging
 + find out why pyspark doesn't freak out when a null is encountered on a field that has nullable=false
--- scheming/__init__.py
+++ scheming/__init__.py
@@ -1,14 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+scheming
+~~~~~~~~~~~~
+This module implements scheming.
+:copyright: (c) 2020 by Steven Docherty.
+:license: Apache2, see LICENSE for more details.
+"""
+
 import json
 from pyspark.sql.types import StructType
 import safer
 import os
 
 
-def get_filename(name, path="./"):
+def get_filename(name: str, path: str = "./") -> str:
     return os.path.join(path, f"{name}.schema.json")
 
 
-def load(schema_filename):
+def load(schema_filename: str) -> (StructType, bool):
     if not os.path.exists(schema_filename):
         return None, True
 
@@ -18,12 +28,16 @@ def load(schema_filename):
     return StructType.fromJson(json.loads(schema_json)), False
 
 
-def save(schema_filename, schema, path="./"):
+def save(schema_filename: str, schema: StructType, path: str = "./"):
     with safer.open(schema_filename, "w") as f:
         f.write(json.dumps(schema.jsonValue(), indent=4))
 
 
-def metadata(schema):
+def metadata(schema: StructType) -> str:
+    r"""Returns the metadata of the schema in a format like printSchema.
+    :param schema: :class:`StructType` schema to be evaluated.
+    :rtype: str
+    """
     str_list = ["root"]
     str_list.append("\n".join([f" |-- {x.name}: {x.metadata}" for x in schema.fields]))
 
