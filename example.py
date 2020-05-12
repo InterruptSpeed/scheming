34d8a6dca6b993351469ca4bf3eec4fb7dd89219
Steven Docherty <sdocherty@InterruptSpeed.com>
2020-05-11 20:07:33 -0400

    applied black to example.py

--- example.py
+++ example.py
@@ -2,10 +2,7 @@ from pyspark.sql import SparkSession
 
 import scheming
 
-spark = (
-    SparkSession.builder.appName("scheming example")
-    .getOrCreate()
-)
+spark = SparkSession.builder.appName("scheming example").getOrCreate()
 
 data_name = "example"
 schema_filename = scheming.get_filename(data_name)
@@ -17,7 +14,7 @@ df = spark.read.csv(csv_filename, header=True, schema=schema, inferSchema=inferS
 if inferSchema:
     scheming.save(schema_filename, df.schema)
     print(f"WARN: inferSchema=True on {csv_filename}")
-    
+
 df.show()
 df.printSchema()
 
